bf1b3f9e49d41ffa1c31b4d2a8780ccd8f45671b
Maksym Diakiv <w68138@student.wsiz.edu.pl>
2023-10-19 15:49:27 +0200

    lab2

---
+++ LAB2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LAB2.Constructions;
+
+namespace LAB2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var construction = new Construction(23, 45, 4, 6, "Bric");
+            Console.WriteLine(construction.Entrances);
+        }
+    }
+}

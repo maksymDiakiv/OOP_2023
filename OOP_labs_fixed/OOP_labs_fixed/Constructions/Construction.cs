3ed879b7f4465715659470aab41cc1199ef26395
maksymdiakiv <dyakiv.max@gmail.com>
2023-12-10 13:36:17 +0100

    lab 2 is complete and fixed

---
+++ OOP_labs_fixed/OOP_labs_fixed/Constructions/Construction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_labs_fixed.Constructions
+{
+    class Construction
+    {
+        public Construction(double height, double width, int entrances, int humanCapacity, string buildMaterial)
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial;
+        }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial
+        {
+            get
+            {
+                return _buildMaterial;
+            }
+            set
+            {
+                _buildMaterial = value;
+                if (_buildMaterial == "")
+                {
+                    throw new ArgumentNullException("_buildMaterial cannot be an empty string");
+                }
+            }
+        }
+        private string _buildMaterial;
+    }
+}
---
+++ OOP_labs_fixed/OOP_labs_fixed/Program.cs
@@ -0,0 +1,16 @@
+﻿using OOP_labs_fixed.Constructions;
+namespace OOP_labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Construction construction = new Construction(50, 25, 4, 200, "wood");
+
+            //Console.WriteLine(construction.HumanCapacity);
+            //new line for git
+            Console.WriteLine(construction.Entrances);
+            Console.WriteLine(construction.Entrances);
+        }
+    }
+}

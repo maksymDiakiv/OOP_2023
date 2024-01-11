1a6d7d3e43a4e90c2853b37d03bf837d8d653165
Maksym Diakiv <w68138@student.wsiz.edu.pl>
2024-01-11 14:37:09 +0100

    commiting from oop2023master full

--- LAB2/Constructions/Construction.cs
+++ LAB2/Constructions/Construction.cs
@@ -1,4 +1,5 @@
-﻿using LAB2.models;
+﻿using LAB2.Constructions.Models;
+using LAB2.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,10 @@ using System.Threading.Tasks;
 
 namespace LAB2.Constructions
 {
-    class Construction
+    class Construction: ISquareCost
     {
-        public Construction(double height, double width, int entrances, int humanCapacity, BuildMaterialType buildMaterial)
+        public Construction(double height, double width, int entrances, 
+            int humanCapacity, BuildMaterialType buildMaterial)
         {
             Height = height;
             Width = width;
@@ -17,14 +19,16 @@ namespace LAB2.Constructions
             HumanCapacity = humanCapacity;
             BuildMaterial = buildMaterial;
         }
+
         public Construction()
         {
             Height = 23;
             Width = 12;
-            Entrances = 6;
-            HumanCapacity = 5;
+            Entrances = 1;
+            HumanCapacity = 1;
             BuildMaterial = BuildMaterialType.Wood;
         }
+
         public Construction(CreateConstructionDto input)
         {
             Height = input.Height;
@@ -33,6 +37,7 @@ namespace LAB2.Constructions
             HumanCapacity = input.HumanCapacity;
             BuildMaterial = input.BuildMaterial;
         }
+
         public double Height { get; set; }
         public double Width { get; set; }
         public int Entrances { get; set; }
@@ -44,13 +49,31 @@ namespace LAB2.Constructions
             }
             set
             {
-                //if(value == string.Empty)
-                //{
-                  //  throw new ArgumentNullException();
-                //}
-                //_buildMaterial = value;
+                _buildMaterial = value;
             }
         }
         private BuildMaterialType _buildMaterial { get; set; }
+
+        public double GetSquareCost()
+        {
+            if (BuildMaterial == BuildMaterialType.Brik)
+            {
+                return Width * Height * 0.9 * 0.8;
+            }
+            if (BuildMaterial == BuildMaterialType.Concrete)
+            {
+                return Width * Height * 0.9 * 0.87;
+            }
+            if (BuildMaterial == BuildMaterialType.Wood)
+            {
+                return Width * Height * 0.9 * 0.78;
+            }
+            return 0;
+        }
+
+        public double CalculateSquareCost()
+        {
+            return GetSquareCost();
+        }
     }
 }
--- LAB2/Program.cs
+++ LAB2/Program.cs
@@ -1,32 +1,48 @@
-﻿using System;
+﻿using LAB2.BuildingWrappers;
+using LAB2.CommercialBuildings;
 using LAB2.Constructions;
-using LAB2.models;
+using LAB2.Constructions.Models;
+using LAB2.IndustrialHalls;
+using LAB2.ResidentalBuildings;
+using System;
 
 namespace LAB2
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
             var construction = new Construction(23, 45, 4, 6, BuildMaterialType.Concrete);
             var construction2 = new Construction();
-            var input = new CreateConstructionDto() { 
+            var input = new CreateConstructionDto()
+            {
                 BuildMaterial = BuildMaterialType.Wood,
                 Width = 23,
-                Height = 10,
-                HumanCapacity = 39,
-                Entrances = 22
-            
-            
+                Height = 12,
+                Entrances = 2,
+                HumanCapacity = 2
             };
-
-            var construction3 = new CreateConstructionDto();
-            Console.WriteLine(construction.Entrances);
-            Console.WriteLine(construction2.Entrances);
-            Console.WriteLine(construction.BuildMaterial);
-            Console.WriteLine(construction2.BuildMaterial);
+            var construction3 = new Construction(input);
             Console.WriteLine(construction3.BuildMaterial);
+            var industrialHall = new IndustrialHall();
+            var residentalBuilding = new ResidentalBuilding(2);
+            Console.WriteLine(industrialHall.BuildMaterial);
+            Console.WriteLine(industrialHall.Height);
+            Console.WriteLine(industrialHall.Width);
+            Console.WriteLine(residentalBuilding.BuildMaterial);
+            Console.WriteLine(residentalBuilding.Height);
+            Console.WriteLine(residentalBuilding.GetSquareCost());
+
+            var wrapper1 = new BuildingWrapper(industrialHall);
+            var wrapper2 = new BuildingWrapper(residentalBuilding);
+            Console.WriteLine(wrapper1.GetWrappedSquareCost());
+            Console.WriteLine(wrapper2.GetWrappedSquareCost());
+
+            var commercialBuilding = new CommercialBuilding(23, 45, 4, 6, BuildMaterialType.Concrete, "Ecommerce");
+            commercialBuilding.OpenBusiness();
+            Console.WriteLine(commercialBuilding.BuildMaterial);
+            Console.WriteLine(commercialBuilding.Height);
+            Console.WriteLine(commercialBuilding.Width);
         }
     }
 }
